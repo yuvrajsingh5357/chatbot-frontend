55580bd028e824e9ee2d65b9192a29e3a4533bee
yuvrajsingh5357 <yuvrajsingh5357@gmail.com>
2024-08-18 20:54:09 +0530

    change color of prompt

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -6,7 +6,7 @@ const ChatMessage = ({ message }) => {
   const isUser = message.type === 'user';
   return (
     <div className={`flex ${isUser ? 'justify-end' : 'justify-start'} mb-4 w-full`}>
-      <div className={`rounded-lg  ${isUser ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black'} px-2 py-1`} style={{ maxWidth: '70%' }}>
+      <div className={`rounded-lg  ${isUser ? 'bg-orange-400 text-white' : 'bg-gray-300 text-black'} px-2 py-1`} style={{ maxWidth: '70%' }}>
        <Markdown>{message.content}</Markdown> 
       </div>
     </div>
